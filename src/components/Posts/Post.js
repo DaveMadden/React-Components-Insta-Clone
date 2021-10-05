b3ace3674e8485a7e76afcfc971bd81776ff0a76
Dave Madden <davelearnsstuff@gmail.com>
2021-10-05 12:26:49 -0700

    MVP complete

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -7,6 +7,8 @@ const Post = props => {
   // 🔥 Make sure the parent of Post is passing the right props!
   const { post, likePost } = props;
 
+  console.log(post.likes);
+
   return (
     <div className='post-border'>
       <PostHeader
@@ -21,9 +23,9 @@ const Post = props => {
         />
       </div>
       {/* Is LikeSection getting all the props it needs to work correctly? */}
-      <LikeSection likePost={() => likePost(post.id)} />
+      <LikeSection likePost={() => likePost(post.id)} numberOfLikes={post.likes} postID={post.id}/> {/* NUMBEROFLIKES IS PROBABLY WRONG HERE */}
       {/* Comments also wants its props! */}
-      <Comments />
+      <Comments comments={post.comments}/> {/* how would i interrogate this? */}
     </div>
   );
 };
